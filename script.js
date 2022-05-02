58f62c40e6b54f26ad0b3d76a812ff0977bac861
Rowings <zakariaazarkan@gmail.com>
2022-05-02 11:21:55 +0200

    ex1 fini

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const h1 = document.querySelector("h1")
+
+let i = 0
+const sentence = "prout"
+
+const myInterval = setInterval(() => {
+    h1.innerHTML += sentence.charAt(i)
+
+    i++
+
+    if(i > sentence.length -1)
+    clearInterval(myInterval)
+}, 1000)
